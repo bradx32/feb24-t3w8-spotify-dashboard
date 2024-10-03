169fa731066bf46bd9ab731406b9bffc7a186050
Bradley Richmond <bradx32@hotmail.com>
2024-10-03 21:13:03 +1000

    feat: signin with spotify works

--- src/contexts/SpotifyAuthProvider.jsx
+++ src/contexts/SpotifyAuthProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 
 export const spotifyAuthScaffold = {
 	access_token: "",
@@ -14,7 +14,7 @@ export function useSpotifyAuthContext(){
 	return useContext(SpotifyAuthContext);
 }
 
-const clientId = "74d0b91062db4b88b881936d03787496";
+const clientId = "14e4e57b7b2b4a93a859cf033350c84f";
 
 export function SpotifyAuthProvider({children}){
 	// Code required for Spotify sign-in process, not usable in API requests
@@ -67,19 +67,69 @@ export function SpotifyAuthProvider({children}){
 
 		// https://api.spotify.com/auth?client_id=oqeihrt90183yrt2ogknjs&code=
 
-		const result = await fetch("https://accoutns.spotify.com/api/token", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded"},
-            body: params
-        });
+		const result = await fetch("https://accounts.spotify.com/api/token", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded"},
+			body: params
+		});
 
-        const authTokens = await result.json();
+		const authTokens = await result.json();
 		return authTokens;
 	}
 
+	// This is the one that sends users to Spotify 
+	async function redirectToAuthCodeFlow() {
+		// Create a security challenge with a verifier value
+		const verifier = generateCodeVerifier(128);
+		const challenge = await generateCodeChallenge(verifier);
+		// persist the verifier for the access token step to use later
+		localStorage.setItem("verifier", verifier);
+	
+		// Configure the API request to begin the auth flow with Spotify
+		const params = new URLSearchParams();
+		params.append("client_id", clientId);
+		params.append("response_type", "code");
+		params.append("redirect_uri", "http://localhost:5173/spotifycallback");
+		params.append("scope", "user-read-private user-read-email");
+		params.append("code_challenge_method", "S256");
+		params.append("code_challenge", challenge);
+	
+		document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+	}
+	
+	// Generates a random alphanumeric value to use as a security codeword
+	function generateCodeVerifier(length) {
+		let text = '';
+		let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	
+		for (let i = 0; i < length; i++) {
+			text += possible.charAt(Math.floor(Math.random() * possible.length));
+		}
+		return text;
+	}
+	
+	// Generates a challenge based on a given "codeVerifier" security codeword 
+	async function generateCodeChallenge(codeVerifier) {
+		const data = new TextEncoder().encode(codeVerifier);
+		const digest = await window.crypto.subtle.digest('SHA-256', data);
+		return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+			.replace(/\+/g, '-')
+			.replace(/\//g, '_')
+			.replace(/=+$/, '');
+	}
+
+
+
+
+
+
+
+
+
+
 
 	return(
-		<SpotifyAuthContext.Provider value={{userAuthData}}>
+		<SpotifyAuthContext.Provider value={{userAuthData, redirectToAuthCodeFlow }}>
 			{children}
 		</SpotifyAuthContext.Provider>
 	);
